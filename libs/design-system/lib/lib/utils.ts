a2884a0ee9dff40cc9a3d53e69a0194edddd3231
Hokki Suwanda <hokkyss2@gmail.com>
2026-08-21 16:46:34 +0700

    refactor: change shadcn theme

--- libs/design-system/lib/lib/utils.ts
+++ libs/design-system/lib/lib/utils.ts
@@ -1,28 +1,6 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-/**
- *
- * @param inputs
- */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function tw(
-  template: TemplateStringsArray,
-): string;
-export function tw(className: string): string;
-/**
- *
- * @param val
- */
-export function tw(
-  val: string | TemplateStringsArray,
-) {
-  if (typeof val === 'string') {
-    return val;
-  }
-
-  return String.raw({ raw: val });
+  return twMerge(clsx(inputs))
 }
